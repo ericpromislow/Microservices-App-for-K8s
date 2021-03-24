20d8f87cf20354f10f290622c503f492a0327468
Eric Promislow <epromislow@suse.com>
2021-03-24 12:11:41 -0700

    Use sqlalchemy to populate the database
    - Add instructions for running the DB with postgres (locally, on macos)

--- app_config.py
+++ app_config.py
@@ -4,4 +4,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = os.environ.get("SECRET_KEY", secrets.token_hex(32))
-DB_URL = os.environ.get("SOCKSESS_DB", 'sqlite:///' + os.path.join(basedir, 'myorders.db'))
+DB_URL = os.environ.get("SOCKSESS_DB", 'sqlite+pysqlite:///' + os.path.join(basedir, 'myorders.db'))
--- info/run.sh
+++ info/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# postgres:
+export SOCKSESS_DB=postgresql+psycopg2://socksess:socksess@127.0.0.1:5432/socksess
+
+# sqlite:
+# export SOCKSESS_DB=sqlite+pysqlite:///$(dirname $(dirname $(realpath "$0")))/myorders.db
 export FLASK_APP=frontend.py
 export FLASK_DEBUG=1
 flask run
